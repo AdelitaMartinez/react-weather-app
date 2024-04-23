69b61c8a103ebdaf626b323be898597ef19da049
Adelita Martinez <amartinez1013@cnm.edu>
2024-04-23 09:49:02 -0600

    Cleaning up code

--- src/api.js
+++ src/api.js
@@ -7,11 +7,3 @@ export const geoApiOption = {
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
-
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
