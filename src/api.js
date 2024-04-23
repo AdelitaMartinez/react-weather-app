3522bba82ab81021559a67c59ed7fd3d62662118
Adelita Martinez <amartinez1013@cnm.edu>
2024-04-23 09:31:21 -0600

    Added GeoDB API

---
+++ src/api.js
@@ -0,0 +1,17 @@
+export const geoApiOption = {
+	method: 'GET',
+	headers: {
+		// 'X-RapidAPI-Key': 'c4e1a6df8emshc612b9e7a0cbce6p19afd8jsn8b5e65e72e7a',
+		// 'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities"
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
