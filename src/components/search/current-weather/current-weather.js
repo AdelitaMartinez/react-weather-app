72b8f30d324f7fe73e0207924b68ea4e7e8f48d2
Adelita Martinez <amartinez1013@cnm.edu>
2024-04-24 14:34:18 -0600

    Added wind and humidity in JS

--- src/components/search/current-weather/current-weather.js
+++ src/components/search/current-weather/current-weather.js
@@ -18,7 +18,15 @@ const CurrentWeather = () => {
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Feels like</span>
-            <span className="parameter-value">Feels like</span>
+            <span className="parameter-value">68°F</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value">2 m/s</span>
+          </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">15%</span>
           </div>
         </div>
       </div>
