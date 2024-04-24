ac22edd9adc4667d428b205b1157cd59a0f204e7
Adelita Martinez <amartinez1013@cnm.edu>
2024-04-24 14:35:15 -0600

    Added pressure

--- src/components/search/current-weather/current-weather.js
+++ src/components/search/current-weather/current-weather.js
@@ -28,6 +28,10 @@ const CurrentWeather = () => {
             <span className="parameter-label">Humidity</span>
             <span className="parameter-value">15%</span>
           </div>
+          <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">15 hPa</span>
+          </div>
         </div>
       </div>
     </div>
