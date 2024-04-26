49aef970bc6a3f3fe5dff902f0e8e4232a670ad9
Adelita Martinez <amartinez1013@cnm.edu>
2024-04-26 14:02:05 -0600

    Added forecast files

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 export const geoApiOption = {
 	method: 'GET',
 	headers: {
-		// 'X-RapidAPI-Key': 'c4e1a6df8emshc612b9e7a0cbce6p19afd8jsn8b5e65e72e7a',
-		// 'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+		'X-RapidAPI-Key': 'c4e1a6df8emshc612b9e7a0cbce6p19afd8jsn8b5e65e72e7a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
 
