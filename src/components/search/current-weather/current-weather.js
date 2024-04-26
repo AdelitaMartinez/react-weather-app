340c90928a21ecf5cd2a80cb5821d77521840314
Adelita Martinez <amartinez1013@cnm.edu>
2024-04-26 13:49:52 -0600

    Added dynamic data to wind speed

--- src/components/search/current-weather/current-weather.js
+++ src/components/search/current-weather/current-weather.js
@@ -18,11 +18,11 @@ const CurrentWeather = ({data}) => {
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Feels like</span>
-            <span className="parameter-value">68°F</span>
+            <span className="parameter-value">(Math.round(data.main.feels_like))°F</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Wind</span>
-            <span className="parameter-value">2 m/s</span>
+            <span className="parameter-value">{data.wind.speed} mph</span>
           </div>
           <div className="parameter-row">
             <span className="parameter-label">Humidity</span>
