c8a091a91fe6e6b51ae85cd1f38a0aefbf4e7bac
Adelita Martinez <amartinez1013@cnm.edu>
2024-04-27 09:06:42 -0600

    Added dynamic details to accordion

--- src/components/search/forecast/forecast.js
+++ src/components/search/forecast/forecast.js
@@ -48,7 +48,34 @@ const Forecast = ({ data }) => {
                 </div>
               </AccordionItemButton>
             </AccordionItemHeading>
-            <AccordionItemPanel></AccordionItemPanel>
+            <AccordionItemPanel>
+              <div className="daily-details-grid">
+                <div className="daily-details-grid-item">
+                  <label>Pressure</label>
+                  <label>{item.main.pressure}</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Humidity</label>
+                  <label>{item.main.humidity}</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Clouds</label>
+                  <label>{item.clouds.all}</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Wind Speed</label>
+                  <label>{item.wind.speed}mph</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Sea level</label>
+                  <label>{item.main.sea_level}m</label>
+                </div>
+                <div className="daily-details-grid-item">
+                  <label>Feels like</label>
+                  <label>{Math.map(item.main.feels_like)}°F</label>
+                </div>
+              </div>
+            </AccordionItemPanel>
           </AccordionItem>
         ))}
       </Accordion>
